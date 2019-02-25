31ada2041d41b9a6434b2f59e26959972a855991
Cari Palazzolo <cari.pizza@gmail.com>
2019-02-24 21:25:41 -0800

    add assets

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Table from './Table';
+import Form from './Form';
+import ApiApp from './Api';
 
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <p>
+//             Edit <code>src/App.js</code> and save to reload.
+//           </p>
+//           <a
+//             className="App-link"
+//             href="https://reactjs.org"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             Learn React
+//           </a>
+//         </header>
+//       </div>
+//     );
+//   }
+// }
 class App extends Component {
+  // state = {
+  //   characters: [
+  //     {
+  //         'name': 'Charlie',
+  //         'job': 'Janitor'
+  //     },
+  //     {
+  //         'name': 'Mac',
+  //         'job': 'Bouncer'
+  //     },
+  //     {
+  //         'name': 'Dee',
+  //         'job': 'Aspring actress'
+  //     },
+  //     {
+  //         'name': 'Dennis',
+  //         'job': 'Bartender'
+  //     }
+  //   ]
+  // };
+  state = {
+    characters: []
+  };
+  removeCharacter = index => {
+    const { characters } = this.state;
+    this.setState({
+        characters: characters.filter((character, i) => { 
+            return i !== index;
+        })
+    });
+  } 
+  handleSubmit = character => {
+    this.setState({characters: [...this.state.characters, character]});
+  }
   render() {
+    const { characters } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello, React!</h1>
+        <Table
+            characterData={characters}
+            removeCharacter={this.removeCharacter} 
+        />
+        <Form handleSubmit={this.handleSubmit} />
+        <ApiApp/>
       </div>
     );
   }
