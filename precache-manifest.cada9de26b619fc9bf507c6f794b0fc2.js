17b42fae5ba3ef51db9c4cdf203d91f4e7a1e508
Cari Palazzolo <cari.pizza@gmail.com>
2019-02-24 21:16:41 -0800

    Updates

---
+++ precache-manifest.cada9de26b619fc9bf507c6f794b0fc2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b37f086e6f9086689ed",
+    "url": "/react-tutorial/static/css/main.a2feb44e.chunk.css"
+  },
+  {
+    "revision": "9b37f086e6f9086689ed",
+    "url": "/react-tutorial/static/js/main.9b37f086.chunk.js"
+  },
+  {
+    "revision": "9b2d15347dbf2dd6be24",
+    "url": "/react-tutorial/static/js/runtime~main.9b2d1534.js"
+  },
+  {
+    "revision": "5411c75cf65661a0dc8c",
+    "url": "/react-tutorial/static/js/2.5411c75c.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-tutorial/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "aa832e216bcd0da16c26efcd07edc15d",
+    "url": "/react-tutorial/index.html"
+  }
+];
